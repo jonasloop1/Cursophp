80be10c0ff95be44483d20fe17369e876ea7fc29
Jonas <jonasloop1@gmail.coim>
2024-10-12 16:13:44 -0300

    Tipos de retorno, Estrutura if, elseif.

--- helpers.php
+++ helpers.php
@@ -1,11 +1,23 @@
 <?php
 
-function saudacao()
+function saudacao(): string
 {
-    return 'Boa tarde';
+    $hora = 6;
+    $saudacao = '';
+
+    if ($hora >= 00 && $hora <= 5) {
+        $saudacao = 'Boa madrugada';
+    } elseif ($hora >= 6 && $hora <= 12) {
+        $saudacao = 'Boa bom dia';
+    } elseif ($hora >= 13 && $hora <= 18) {
+        $saudacao = 'Boa tarde';
+    } else {
+        $saudacao = 'Boa noite';
+    }
+    return $saudacao;
 }
 
-function resumirTexto($texto, $limite, $continue = '...')
+function resumirTexto(string $texto, int $limite, string $continue = '...'): string //":string" definir tipo de retorno!
 {
-    return $texto.$continue;
+    return $texto . $continue;
 }
--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 //Arquivo responsavel pela inicialização do sistema
-
+//declare(strict_types = 1); faz com que tipos de varias diferentes das declaradas não sejam aceitas por outros tipos!
 include_once 'sistemas/configuracao.php';
 include_once 'helpers.php';
 
 $texto = 'texto para resumir vindo uma variavel';
 
+$string = 'TEXTO';
+$int = 01;
+$float = 3.3;
+$bool = true;
+$nulo = null; // diferente de uma string VAZIA 
+
+var_dump($texto); // Serve para debugar uma variavel, classe, funcão ou objeto... 
+echo '<hr>';
 echo saudacao();
 echo '<hr>';
 echo resumirTexto($texto, 10);
