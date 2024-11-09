4becd3adfc490f6b922d09ec3d56ebb1dbc8afcd
Jonas <jonasloop1@gmail.coim>
2024-11-09 17:33:06 -0300

    TimeZone

--- Aula 22-/index2.php
+++ Aula 22-/index2.php
@@ -1,10 +1,14 @@
 <?php
 include_once 'helpers2.php';
+require_once 'sistemas/configuracao.php';  
 
 echo 'R$ '.formatarValor();
 echo '<hr>';
 echo formatarNumero(10000000);
+echo '<hr>';
 
+$data = date('d/m/y H.i.s');
+echo $data;
 
 //$texto = ' <h1>texto</h1> <p>para resumir vindo</p> de uma variavel ';
 //$texto = strip_tags($texto);//Limpa todas as TAGS!
--- sistemas/configuracao.php
+++ sistemas/configuracao.php
@@ -1,2 +1,3 @@
 <?php 
 // Arquivo de configuração do sistema
+date_default_timezone_set('America/Sao_Paulo');
